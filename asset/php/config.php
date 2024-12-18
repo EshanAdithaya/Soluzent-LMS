bbd2fde8324eef2f3f1c8b794d8aef1b582007fa
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2024-12-18 09:23:15 +0530

    updates

--- asset/php/config.php
+++ asset/php/config.php
@@ -5,7 +5,8 @@ if (session_status() === PHP_SESSION_NONE) {
     session_set_cookie_params([
         'lifetime' => 3600,
         'path' => $session_path,
-        'domain' => parse_url('https://plankton-app-us3aj.ondigitalocean.app', PHP_URL_HOST),
+        // 'domain' => parse_url('https://plankton-app-us3aj.ondigitalocean.app', PHP_URL_HOST),
+        'domain' => parse_url('http://161.35.63.178', PHP_URL_HOST),
         'secure' => true,
         'httponly' => true,
         'samesite' => 'Strict'
@@ -33,7 +34,8 @@ define('DB_USER', 'doadmin');
 define('DB_PASS', 'AVNS_l3SW8eljPIvmmGNUCFK');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_PORT', 25060);
-define('APP_URL', 'https://plankton-app-us3aj.ondigitalocean.app');
+// define('APP_URL', 'https://plankton-app-us3aj.ondigitalocean.app');
+define('APP_URL', 'http://161.35.63.178');
 
 // Application Settings
 define('APP_NAME', 'EduPortal');
